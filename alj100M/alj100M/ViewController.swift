96f0c0d43c9a5fdf614a9b520f854a951487b758
Owner <owner@MA3012.local>
2020-06-08 16:41:27 +0900

    added alj100M

---
+++ alj100M/alj100M/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  alj100M
+//
+//  Created by Owner on 2020/06/08.
+//  Copyright © 2020 asOne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func backView(segue: UIStoryboardSegue) {
+        
+    }
+
+}
+
