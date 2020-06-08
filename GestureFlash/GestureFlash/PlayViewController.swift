75a5fb6fb48ac46b8ec1b5e29d88806aa7ebdb0a
Owner <owner@MA3012.local>
2020-06-08 16:43:54 +0900

    added GestureFlash

--- GestureFlash/GestureFlash/PlayViewController.swift
+++ GestureFlash/GestureFlash/PlayViewController.swift
@@ -8,23 +8,169 @@
 
 import UIKit
 
-class PlayViewController: UIViewController {
+class PlayViewController: UIViewController, UIGestureRecognizerDelegate {
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var completedGestureLabel: UILabel!
+    @IBOutlet weak var gestureImage: UIImageView!
+    
+    var startTime = NSDate()
+    var completedGestures = Int()
+    var currentGesture = Int()
+    var timer = Timer()
+    var timerCount = Double()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        completedGestures = 0
+        
+        var swipeRightSelector = #selector(self.swipeRightDetected(sender:))
+        var swipeRightGestureRecognizer = UISwipeGestureRecognizer(target: self, action: swipeRightSelector)
+        var swipeLeftSelecter = #selector(self.swipeLeftDetected(sender:))
+        var swipeLeftGestureRecognizer = UISwipeGestureRecognizer(target: self, action: swipeLeftSelecter)
+        var swipeUpSelector = #selector(self.swipeUpDetected(sender:))
+        var swipeUpGestureRecognizer = UISwipeGestureRecognizer(target: self, action: swipeUpSelector)
+        var swipeDownSelector = #selector(self.swipeDownDetected(sender:))
+        var swipeDownGestureRecognizer = UISwipeGestureRecognizer(target: self, action: swipeDownSelector)
+        var pinchSelector = #selector(self.pinchDetected(sender:))
+        var pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: pinchSelector)
+        var rotateSelector = #selector(self.rotateDetected(sender:))
+        var rotateGestureRecognizer = UIRotationGestureRecognizer(target: self, action: rotateSelector)
+        self.view.addGestureRecognizer(swipeRightGestureRecognizer)
+        self.view.addGestureRecognizer(swipeLeftGestureRecognizer)
+        self.view.addGestureRecognizer(swipeUpGestureRecognizer)
+        self.view.addGestureRecognizer(swipeDownGestureRecognizer)
+        self.view.addGestureRecognizer(pinchGestureRecognizer)
+        self.view.addGestureRecognizer(rotateGestureRecognizer)
+        
+        self.nextProblem()
+        
+        timerCount = 0
+        timer = Timer.scheduledTimer(timeInterval: 0.1,
+                                     target: self,
+                                     selector: #selector(PlayViewController.onTimer(timer:)),
+                                     userInfo: nil,
+                                     repeats: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        var elapsedTime = startTime.timeIntervalSinceNow
+        elapsedTime = -(elapsedTime)
+        
+        if segue.identifier == "toResultView" {
+            let rvc = segue.destination as! ResultViewController
+            rvc.time = elapsedTime
+        }
     }
-    */
+    
+    func nextProblem() {
+        if completedGestures == 30 {
+            self.performSegue(withIdentifier: "toResultView", sender: self)
+        } else {
+            var gestureIcons = [UIImage(named: "swipe-right"),
+                                UIImage(named: "swipe-left"),
+                                UIImage(named: "swipe-up"),
+                                UIImage(named: "swipe-down"),
+                                UIImage(named: "pinch-in"),
+                                UIImage(named: "pinch-out"),
+                                UIImage(named: "rotate-right"),
+                                UIImage(named: "rotate-left")
+            ]
+            
+            currentGesture = Int(arc4random() % 8)
+            NSLog("got new gesture current: %d", currentGesture)
+            gestureImage.image = gestureIcons[currentGesture]
+            completedGestureLabel.text = String(format: "%d", completedGestures)
+        }
+    }
+    
+    @objc func onTimer(timer: Timer) {
+        timerCount = timerCount + 0.1
+        timeLabel.text = String(format: "%.1f", timerCount)
+    }
+    
+    @objc func swipeRightDetected(sender: UISwipeGestureRecognizer) {
+        NSLog("swipe-right")
+        NSLog("current: %d", currentGesture)
+        if currentGesture == 0 {
+            NSLog("next")
+            completedGestures += 1
+            self.nextProblem()
+        }
+    }
+    
+    @objc func swipeLeftDetected(sender: UISwipeGestureRecognizer) {
+        NSLog("swipe-left")
+        NSLog("current: %d", currentGesture)
+        if currentGesture == 1 {
+            NSLog("next")
+            completedGestures += 1
+            self.nextProblem()
+        }
+    }
+    
+    @objc func swipeUpDetected(sender: UISwipeGestureRecognizer) {
+        NSLog("swipe-up")
+        NSLog("current: %d", currentGesture)
+        if currentGesture == 2 {
+            NSLog("next")
+            completedGestures += 1
+            self.nextProblem()
+        }
+    }
+    
+    @objc func swipeDownDetected(sender: UISwipeGestureRecognizer) {
+        NSLog("swipe-down")
+        NSLog("current: %d", currentGesture)
+        if currentGesture == 3 {
+            NSLog("next")
+            completedGestures += 1
+            self.nextProblem()
+        }
+    }
+    
+    @objc func pinchDetected(sender: UIPinchGestureRecognizer) {
+        let scale = sender.scale
+        if scale < 0.4 {
+            NSLog("pinch-in scale: %f", scale)
+            NSLog("current: %d", currentGesture)
+            if currentGesture == 4 {
+                NSLog("next")
+                completedGestures += 1
+                self.nextProblem()
+            }
+        } else if scale > 2.4 {
+            NSLog("pinch-out scale: %f", scale)
+            NSLog("current: %d", currentGesture)
+            if currentGesture == 5 {
+                NSLog("next")
+                completedGestures += 1
+                self.nextProblem()
+            }
+        }
+    }
+    
+    @objc func rotateDetected(sender: UIRotationGestureRecognizer) {
+        let radians = sender.rotation
+        let degrees = radians * CGFloat(100 / M_PI)
+        if degrees > 90 {
+            NSLog("rotate-right degrees: %f", degrees)
+            NSLog("current: %d", currentGesture)
+            if currentGesture == 6 {
+                NSLog("next")
+                completedGestures += 1
+                self.nextProblem()
+            }
+        } else if degrees < -90 {
+            NSLog("rotate-left degrees: %f", degrees)
+            NSLog("current: %d", currentGesture)
+            if currentGesture == 7 {
+                NSLog("next")
+                completedGestures += 1
+                self.nextProblem()
+            }
+        }
+    }
 
 }
--- GestureFlash/GestureFlash/ResultViewController.swift
+++ GestureFlash/GestureFlash/ResultViewController.swift
@@ -10,21 +10,55 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var newHighScoreLabel: UILabel!
+    
+    var time = TimeInterval()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        timeLabel.text = String(format: "%.3f 秒", time)
+        self.checkHighScore()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func checkHighScore() {
+        var newHighScore = false
+        newHighScoreLabel.isHidden = true
+        
+        let defaults = UserDefaults.standard
+        var highScore1 = defaults.double(forKey: "highScore1")
+        var highScore2 = defaults.double(forKey: "highScore2")
+        var highScore3 = defaults.double(forKey: "highScore3")
+        
+        if highScore1 != 0 && time <= highScore1 {
+            highScore3 = highScore2
+            highScore2 = highScore1
+            highScore1 = time
+            newHighScore = true
+        } else if highScore2 != 0 && time <= highScore2 {
+            highScore3 = highScore2
+            highScore2 = time
+            newHighScore = true
+        } else if highScore3 != 0 && time <= highScore3 {
+            highScore3 = time
+            newHighScore = true
+        } else if highScore1 == 0 {
+            highScore1 = time
+            newHighScore = true
+        } else if highScore2 == 0 && time >= highScore1 {
+            highScore2 = time
+            newHighScore = true
+        } else if highScore3 == 0 && time >= highScore2 {
+            highScore3 = time
+            newHighScore = true
+        }
+        
+        defaults.set(highScore1, forKey: "highScore1")
+        defaults.set(highScore2, forKey: "highScore2")
+        defaults.set(highScore3, forKey: "highScore3")
+        
+        if newHighScore == true {newHighScoreLabel.isHidden = false}
     }
-    */
-
+    
 }
