61a3e9669ea86648d16166312ed9455262d33c9f
Owner <owner@MA3012.local>
2020-06-12 11:09:38 +0900

    add Messenger

---
+++ Messenger/Messenger/Messenger-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Messenger-Bridging-Header.h
+//  Messenger
+//
+//  Created by Owner on 2020/06/11.
+//  Copyright © 2020 asOne. All rights reserved.
+//
+
+#ifndef Messenger_Bridging_Header_h
+#define Messenger_Bridging_Header_h
+
+#import <NCMB/NCMB.h>
+#endif /* Messenger_Bridging_Header_h */
