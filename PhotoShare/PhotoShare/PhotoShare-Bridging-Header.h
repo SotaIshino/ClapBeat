a8ac17894266a7aad8965e18d03bc5a55922dc11
Owner <owner@MA3012.local>
2020-06-15 10:51:33 +0900

    add PhotoShare

---
+++ PhotoShare/PhotoShare/PhotoShare-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  PhotoShare-Bridging-Header.h
+//  PhotoShare
+//
+//  Created by Owner on 2020/06/12.
+//  Copyright © 2020 asOne. All rights reserved.
+//
+
+#ifndef PhotoShare_Bridging_Header_h
+#define PhotoShare_Bridging_Header_h
+
+#import <NCMB/NCMB.h>
+#endif /* PhotoShare_Bridging_Header_h */
